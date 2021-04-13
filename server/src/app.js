ff2d2c71ceacae68418cce7571c3004bbd75da75
MDZ-0 <79402655+MDZ-0@users.noreply.github.com>
2021-04-13 13:26:38 +0000

    added exprexpress server

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,21 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const port = process.env.PORT || 3000
 
 // App container
 const app = express()
-app.use(morgan('combined'))
+app.use(cors())
+app.options("*", cors())
 app.use(bodyParser.json())
-app.use(cors)
+app.use(morgan('combined'))
 
-
-app.get('status', (res,req) => {
-
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello world'
+  })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
